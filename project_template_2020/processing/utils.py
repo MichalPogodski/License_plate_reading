Detected characters saved in dict (wrong order)

project_template_2020/processing/utils.py
@@ -12,6 +12,7 @@ def contour_to_rect(image):
     img_threshed = cv.adaptiveThreshold(blurred,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY,11,2)
     edged = cv.Canny(img_threshed, 30, 200)
     contours, hierarchy = cv.findContours(edged, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    cv.drawContours(img, contours, -1, (0, 255, 0), 3)
     cnts = imutils.grab_contours([contours, hierarchy])
     cnts = sorted(cnts, key=cv.contourArea, reverse=True)[:10]
     screenCnt = None
@@ -24,8 +25,11 @@ def contour_to_rect(image):
             screenCnt = approx
             break
     cv.drawContours(img, [screenCnt], 0, (0, 255, 0), 1)
-    # cv.imshow('test', img)
     p0, p2, p3, p1 = screenCnt[0][0], screenCnt[1][0], screenCnt[2][0], screenCnt[3][0]
+    p0 = [p0[0] - 5, p0[1] - 5]
+    p1 = [p1[0] + 5, p1[1] - 5]
+    p2 = [p2[0] - 5, p2[1] + 5]
+    p3 = [p3[0] + 5, p3[1] + 5]
 
     rect = np.array([p0, p1, p2, p3], np.float32)
     dst = np.array([[0, 0], [600, 0], [0, 150], [600, 150]], np.float32)
@@ -38,14 +42,35 @@ def contour_to_rect(image):
 
 
 
-
 def thresh_chars(img):
-    #preproc of image, for characters recognition
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     img = cv.bilateralFilter(gray, 101, 29, 29)
     ret, t1 = cv.threshold(img, 200, 255, cv.THRESH_BINARY)
 
-    cv.imshow('1', t1)
+    return t1
+
+
+def segment(img, clr):
+    edged = cv.Canny(img, 30, 200)
+    contours, hierarchy = cv.findContours(edged, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    characters = {}
+    i, j = 0, 0
+    for cnt in contours:
+        x, y, w, h = cv.boundingRect(cnt)
+        if 1.0 < h/w < 2.5 and h >= 100 and j == 0:
+            # cv.rectangle(clr, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cp =img.copy()
+            ch = cp[y:y+h, x:x+w]
+            characters[i] = ch
+            i += 1
+            j = 1
+        else:
+            j = 0
+    for key in characters:
+        cv.imshow(str(key), characters[key])
+
+
+
 
 
 
@@ -53,7 +78,11 @@ def perform_processing(image: np.ndarray) -> str:
     print(f'image.shape: {image.shape}')
     # TODO: add image processing here
     plate = contour_to_rect(image)
-    thresh_chars(plate)
+    threshed = thresh_chars(plate)
+    segment(threshed, plate)
+
+
+
     cv.waitKey()
 
     return 'PO12345'