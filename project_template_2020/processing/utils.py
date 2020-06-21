3b3588a0e1071f7153ef4fa46e9b4d295828d020
MichalPogodski <pogodskimichal@gmail.com>
2020-06-21 22:30:32 +0200

    fix plate detection

--- project_template_2020/processing/utils.py
+++ project_template_2020/processing/utils.py
@@ -3,33 +3,71 @@ import cv2 as cv
 import os
 from skimage import measure
 
+not_det = 0
 
 def find_plate(img):
-    x_size, y_size, ch = img.shape
-    a = int(x_size/110)
-    if a % 2 == 0: a += 1
-    b = int(x_size/176)
-    if b % 2 == 0: b += 1
-    c = int(x_size/230)
-    if c % 2 == 0: c += 1
 
     to_ret = img.copy()
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    blurred_pre = cv.GaussianBlur(gray, (a, a), 1)
-    blurred = cv.bilateralFilter(blurred_pre, c, b, b)
+    blurred = cv.medianBlur(gray, 3)
     _, thresh = cv.threshold(blurred, 145, 255, cv.THRESH_BINARY_INV)
     contours, _ = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
     hull = []
     for cnt in contours: hull.append(cv.convexHull(cnt))
-    cnts = sorted(hull, key=cv.contourArea, reverse=True)
+    cnts = sorted(hull, key=cv.contourArea, reverse=True)[:10]
     box = None
 
     for i in range(len(cnts)):
-        epsilon = 0.02 * cv.arcLength(cnts[i], True)
+        epsilon = 0.03 * cv.arcLength(cnts[i], True)
         apprx = cv.approxPolyDP(cnts[i], epsilon, True)
         if len(apprx) == 4:
             x, y, w, h = cv.boundingRect(apprx)
-            if 2.0 <= w/h <= 5.75 and w >= img.shape[0]/3:
+            if 2.5 <= w/h <= 7.5 and w >= img.shape[0]/3 and w < img.shape[0]:
+                box = apprx
+                break
+
+    p0, p2, p3, p1 = box[0][0], box[1][0], box[2][0], box[3][0]
+    corn = []
+    dict = {}
+    dict[sum(p0)] = p0
+    dict[sum(p1)] = p1
+    dict[sum(p2)] = p2
+    dict[sum(p3)] = p3
+    srt_crn = sorted(dict)
+
+    for idx in range(4):
+        corn.append(dict[srt_crn[idx]])
+
+    p0 = [corn[0][0], corn[0][1] - 15]
+    p1 = [corn[2][0], corn[2][1] - 15]
+    p2 = [corn[1][0], corn[1][1] + 15]
+    p3 = [corn[3][0], corn[3][1] + 15]
+
+    rect = np.array([p0, p1, p2, p3], np.float32)
+    dst = np.array([[0, 0], [600, 0], [0, 150], [600, 150]], np.float32)
+    M = cv.getPerspectiveTransform(rect, dst)
+    warped_clean = cv.warpPerspective(to_ret, M, (600, 150))
+
+    return warped_clean
+
+
+def adaptive_find_plate(img):
+    to_ret = img.copy()
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    blurred = cv.medianBlur(gray, 9)
+    thresh = cv.adaptiveThreshold(blurred, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY_INV, 21, 2)
+    contours, _ = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    hull = []
+    for cnt in contours: hull.append(cv.convexHull(cnt))
+    cnts = sorted(hull, key=cv.contourArea, reverse=True)[:10]
+    box = None
+
+    for i in range(len(cnts)):
+        epsilon = 0.03 * cv.arcLength(cnts[i], True)
+        apprx = cv.approxPolyDP(cnts[i], epsilon, True)
+        if len(apprx) == 4:
+            x, y, w, h = cv.boundingRect(apprx)
+            if 2.5 <= w / h <= 7.5 and w >= img.shape[0] / 3 and w < img.shape[0]:
                 box = apprx
                 break
 
@@ -139,28 +177,65 @@ def perf_comparison(segmented):
 def perform_processing(image: np.ndarray) -> str:
     print(f'image.shape: {image.shape}')
     # TODO: add image processing here
+    global not_det
 
     try:
         plate = find_plate(image)
     except:
-        recognition = '#######'
+
+        try:
+            plate = adaptive_find_plate(image)
+        except:
+            recognition = '???????'
+            not_det += 1
+        else:
+            threshed = thresh_chars(plate)
+            cv.imshow('detected plate_ADAPT******', threshed)
+
+            try:
+                segmented = segment(threshed)
+                recognition = perf_comparison(segmented)
+            except:
+                recognition = '???????'
 
     else:
         threshed = thresh_chars(plate)
+        cv.imshow('detected plate', threshed)
+
         try:
             segmented = segment(threshed)
             recognition = perf_comparison(segmented)
         except:
-            recognition = '#######'
+            recognition = '???????'
+
+
+
+    if len(recognition) != 7:
+        try:
+            plate = adaptive_find_plate(image)
+        except:
+            recognition = '???????'
+            not_det += 1
+
+        else:
+            threshed = thresh_chars(plate)
+            cv.imshow('detected plate_ADAPT******', threshed)
+            try:
+                segmented = segment(threshed)
+                recognition = perf_comparison(segmented)
+            except:
+                recognition = '???????'
+
 
     if len(recognition) < 7:
         for i in range(7 - len(recognition)):
-            recognition += '#'
+            recognition += '?'
     elif len(recognition) > 7:
         rec = recognition
         recognition = rec[(len(rec)-7):]
 
-    #print('recognition ', recognition)
+    print('recognition ', recognition)
     cv.waitKey()
+    print('not detected: ', not_det)
 
     return recognition
